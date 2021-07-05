647bab118fd1ffb786f630daa4950a8fcb99fa69
Ghazal <ghazal.kazemi@gmail.com>
2021-07-05 13:11:02 +1000

    Make the api read-only by disabling httpMethods

---
+++ src/main/java/com/springboot/ecommerce/config/AppDataRestConfig.java
@@ -0,0 +1,26 @@
+package com.springboot.ecommerce.config;
+
+import com.springboot.ecommerce.entity.Product;
+import com.springboot.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+@Configuration
+public class AppDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedMethods = {HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PUT};
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods)));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods)));
+
+    }
+}
